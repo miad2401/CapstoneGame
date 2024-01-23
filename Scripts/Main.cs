505b9a7dc752a6bdabf2e5a6fa52aa24a1d3a9e2
miad2401 <miad2401@gmail.com>
2024-01-23 13:01:14 -0500

    Added some logic for managing resource tiles

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections;
 
 public partial class Main : Node2D
 {
@@ -32,10 +33,29 @@ public partial class Main : Node2D
 	int employedPopVal;
 	double growthVal;
 
+	[Export] TileMap regionMap;
+	struct ResourceNode { int xPos; int yPos; bool activated; bool worked;
+		public ResourceNode(int x, int y, bool active, bool working)
+		{
+			xPos = x; yPos = y; activated = active; worked = working;
+		}
+	};
+	ArrayList resourceNodes = new ArrayList();
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		//Here we would set our inital values for our resources, based on difficulty selected.
+
+		//Also populate our list of resource nodes with all current resource nodes
+		Vector2I resourceAtlasLocation = new Vector2I(4, 0);
+		Godot.Collections.Array<Vector2I> resourceArray = regionMap.GetUsedCellsById(0, 0, resourceAtlasLocation);
+
+		foreach(Vector2I cell in resourceArray)
+		{
+			ResourceNode currNode = new ResourceNode(cell.X, cell.Y, false, false);
+			resourceNodes.Add(currNode);
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
